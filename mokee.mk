765260220da68ebb220b81dd94b3d059593b115d
martincz <martincz@mokeedev.com>
2014-05-07 16:45:30 +0800

    mokee bringup
    Change-Id: I372bff3bc3679abd9bf7db058b6408f815019889

---
+++ mokee.mk
@@ -0,0 +1,9 @@
+$(call inherit-product, device/htc/m8/full_m8.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
+
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+
+PRODUCT_NAME := mk_m8
