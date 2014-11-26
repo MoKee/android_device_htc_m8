64e2dd0293b63368d52599da5c295cab65ec1d6d
martincz <martincz@mokeedev.com>
2014-11-26 15:51:45 +0800

    m8: mokee bringup
    Change-Id: Iaa0133ef3210ee7d7903f668f6b0c9a38f4986e8

--- dotcase/src/org/cyanogenmod/dotcase/Dotcase.java
+++ dotcase/src/org/cyanogenmod/dotcase/Dotcase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 The MoKee OpenSource Project
+ * Copyright (c) 2014 The CyanogenMod Project
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
