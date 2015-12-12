3b8a272bd1f5df87f0c362525cb6b7f3cfc51150
martincz <martincz@mokeedev.com>
2015-12-13 05:02:31 +0800

    m8: mokee bringup

--- mokee.mk
+++ mokee.mk
@@ -17,9 +17,9 @@
 $(call inherit-product, device/htc/m8/full_m8.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_m8
+PRODUCT_NAME := mk_m8
