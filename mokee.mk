85a17d5f86e539fb9b32daa5516fe9c50f76704d
martincz <martincz@mokeedev.com>
2016-11-29 15:12:35 +0800

    m8: mokee bringup
    Change-Id: I344447537175a008e5a9efedb9cbcd31481d6383

--- mokee.mk
+++ mokee.mk
@@ -17,11 +17,11 @@
 # Inherit from m8 device
 $(call inherit-product, device/htc/m8/device.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := cm_m8
+PRODUCT_NAME := mk_m8
 PRODUCT_DEVICE := m8
 PRODUCT_BRAND := htc
 PRODUCT_MANUFACTURER := htc
